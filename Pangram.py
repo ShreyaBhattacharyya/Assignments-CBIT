f49f113f6a15c84f48cfe916df1bc23ff2864ba8
ShreyaBhattacharyya <69763694+ShreyaBhattacharyya@users.noreply.github.com>
2020-08-22 17:59:43 +0530

    Update Pangram.py

--- Pangram.py
+++ Pangram.py
@@ -1,3 +1,6 @@
+#This code is to check whether the entered string is a pangram string or not
+#A pangram string is one that contains all the letters of the English alphabet 
+
 def checkpangram(string):
     alphabet = ['a','b','c','d','e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #storing the alphabet in an array
     for char in alphabet:
